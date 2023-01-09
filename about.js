dedbd54dbc86d6062ef51dabf6d3b6312978d813
MayumiPARK <118215314+Yumicode@users.noreply.github.com>
2023-01-09 10:38:15 -0700

    Add files via upload

---
+++ about.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('Form has been submitted successfully!!');
+}
+
+function imgCatover(){
+
+	let form = document.querySelector('#contact');
+}
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
